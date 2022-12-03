040279aeed354a746ff2ee258b3fdc4a93bd988a
Olajide <clickcom007@yahoo.com>
2022-12-03 16:43:06 +0100

    install social share

--- price_compare/settings.py
+++ price_compare/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'category',
+    'accounts',
+    'django_social_share',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
