aa8e45f57faf9c2c021c18417bf66d144a95055a
Olajide <clickcom007@yahoo.com>
2022-12-06 13:51:33 +0100

    add product app

--- price_compare/settings.py
+++ price_compare/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'category',
     'search',
     'accounts',
-    # 'products.apps.ProductsConfig',
+    'products.apps.ProductsConfig',
     'django_social_share',
     'django.contrib.admin',
     'django.contrib.auth',
